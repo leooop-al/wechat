10c52691908cf6a51cb6e7d4b1186dd06cb0ce51
seuers_leo <389141994@qq.com>
2023-06-12 08:46:58 +0800

    merge old version

--- src/main/java/com/tencent/wxcloudrun/robot/request/ChatRequest.java
+++
@@ -1,8 +0,0 @@
-package com.tencent.wxcloudrun.robot.request;
-
-/**
- * @author ziyou.cxf
- * @version : ChatRequest.java, v 0.1 2023年06月08日 9:09 ziyou.cxf Exp $
- */
-public class ChatRequest {
-}
