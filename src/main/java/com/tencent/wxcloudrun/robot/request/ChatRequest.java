66ebf154df5a8b7ff561b91d4dd36c57c17369af
seuers_leo <389141994@qq.com>
2023-06-12 08:46:03 +0800

    wechat development

---
+++ src/main/java/com/tencent/wxcloudrun/robot/request/ChatRequest.java
@@ -0,0 +1,8 @@
+package com.tencent.wxcloudrun.robot.request;
+
+/**
+ * @author ziyou.cxf
+ * @version : ChatRequest.java, v 0.1 2023年06月08日 9:09 ziyou.cxf Exp $
+ */
+public class ChatRequest {
+}
